7fa514e3e5d5c686fbf0484f847c78ecd09a16fb
Zereth <70236765+o5zereth@users.noreply.github.com>
2021-10-11 17:40:15 -0400

    Create base plugin + add EXILED reference.

---
+++ Loader.cs
@@ -0,0 +1,26 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using System;
+
+namespace GamemodeManager
+{
+    public class Loader : Plugin<Config>
+    {
+        public override string Author => "Zereth";
+        public override string Name => "GamemodeManager";
+        public override string Prefix => "gamemode_manager";
+        public override PluginPriority Priority => PluginPriority.Default;
+        public override Version RequiredExiledVersion => new Version(3, 0, 5);
+        public override Version Version => new Version(1, 0, 0);
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+    }
+}
